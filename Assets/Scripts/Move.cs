d498ac9ccceadd7fcaf629a3c076d197cf084576
HenryCollins14 <60672325+HenryCollins14@users.noreply.github.com>
2021-01-14 12:19:15 -0800

    Final Project Complete

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Move : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    public float speed;
+    float WPradius = 1;
+
+    private void Update()
+    {
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+
+}
